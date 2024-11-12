4e3c1cd3d735e765b98c9e9caf7f504546d95bab
Sunkar-go <152873637+Sunkar-go@users.noreply.github.com>
2024-11-12 17:00:20 +0500

    From 12 november evening

--- first_file.py
+++ first_file.py
@@ -1,1 +1,9 @@
-print("hello World")
+import sys
+import os
+from PIL import Image
+
+from_Path_folder=sys.argv[1]
+to_Path_Folder=sys.argv[2]
+for i in os.listdir(from_Path_folder):
+	if os.path.isdir(to_Path_Folder):
+		i.save(f"{i}.png",'png')
